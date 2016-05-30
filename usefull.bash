ad95ccd14a2bc14f233636972d2e94141d2fa50c
alex <support@mazurov.eu>
2016-05-30 08:30:15 +0300

    vdev

--- usefull.bash
+++ usefull.bash
@@ -1,4 +1,4 @@
 git add -A
 git commit -m "vdev"
-git pull
-git config remote.origin.url https://support@sinelga.com:******@github.com/sinelga/horoscope.git
+git push
+#git config remote.origin.url https://sinelga:******@github.com/sinelga/horoscope.git
